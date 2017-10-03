03367d801160c54001823dfdb59b3daa918ea3a9
Alberto Delgado <adelgado.roda@gmail.com>
2017-10-03 20:08:31 +0200

    Create Espresso class

---
+++ decorator_pattern/CondimentDecorator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 03/10/17
+ * Time: 19:26
+ */
+
+class CondimentDecorator
+{
+
+}
---
+++ decorator_pattern/Espresso.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberto
+ * Date: 03/10/17
+ * Time: 20:06
+ */
+
+class Espresso
+{
+
+}
