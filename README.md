28280ab42038817901876cd350d3ca8e5bdac85d
Alberto Delgado <adelgado.roda@gmail.com>
2017-10-03 22:16:48 +0200

    Code where I will apply the refactor to factory pattern

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ I'm working with the next design patterns:
 1. Strategy pattern
 2. Observer pattern
 3. Decorator pattern
-
+4. Factory pattern
---
+++ factory_pattern/OrderPizza.php
@@ -0,0 +1,16 @@
+<?php
+
+class PizzaStore
+{
+    public function orderPizza(): Pizza
+    {
+        $pizza = new Pizza();
+
+        $pizza->prepare();
+        $pizza->bake;
+        $pizza->cut();
+        $pizza->box();
+
+        return $pizza;
+    }
+}
