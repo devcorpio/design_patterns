ca22362ef88b2f52a65596983cedbaf32f83c943
Alberto Delgado <adelgado.roda@gmail.com>
2017-10-03 23:25:53 +0200

    creating the pizza hierarchy!

---
+++ factory_pattern/factory_method/NYStyleCheesePizza.php
@@ -0,0 +1,11 @@
+<?php
+
+class NYStyleCheesePizza extends Pizza
+{
+    public function __construct()
+    {
+        $this->name = "NY Style Sace and Cheese Pizza";
+        $this->dough = "Thin Crust Dough";
+        $this->sauce = "Marinara Sauce";
+    }
+}
