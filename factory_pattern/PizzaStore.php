00b370fcaca35ac3d4eb99b7b9b780505411c2e0
Alberto Delgado <adelgado.roda@gmail.com>
2017-10-03 22:36:31 +0200

    Creating pizza factory, now, if there are more type of pizzas, this code will not be alterated

--- factory_pattern/PizzaStore.php
+++ factory_pattern/PizzaStore.php
@@ -2,19 +2,19 @@
 
 class PizzaStore
 {
+    /**
+     * @var SimplePizzaFactory
+     */
+    private $factory;
+
+    public function __construct(SimplePizzaFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
     public function orderPizza(string $type): Pizza
     {
-        $pizza = null;
-
-        if ($type === "cheese") {
-            $pizza = new CheesePizza();
-        } else if ($type === "pepperoni") {
-            $pizza = new PepperoniPizza();
-        } else if ($type === "clam") {
-            $pizza = new ClamPizza();
-        } else if ($type === "veggie") {
-            $pizza = new VeggiePizza();
-        }
+        $pizza = $this->factory->createPizza($type);
 
         $pizza->prepare();
         $pizza->bake();
